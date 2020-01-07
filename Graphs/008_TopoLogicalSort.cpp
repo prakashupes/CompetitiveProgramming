52e552e7a6ee3766c6a86bcf17a0408421854a3b
prakashupes <85prakash2017@gmail.com>
2020-01-07 19:53:13 +0530

    =Nodes

--- Graphs/008_TopoLogicalSort.cpp
+++ Graphs/008_TopoLogicalSort.cpp
@@ -27,15 +27,17 @@ void dfs(unordered_map<int,list<int>> adjList)
 
     //Visit node one by one
     //adjList[0], represents first independent node
-    for(auto x:adjList)
+for(auto x:adjList)
     {
         int node=x.first;
         if(!visited[node])
-        {
+        {   cout<<node<<" ";
             dfsHelper(node,adjList,visited,topoShort);
         }
 
     }
+
+    cout<<endl;
 
 
    // topoShort.push_front(src);
@@ -50,13 +52,14 @@ void dfs(unordered_map<int,list<int>> adjList)
 int main()
 {
     unordered_map<int,list<int>> adjList;
+    adjList[1].push_back(4);
     adjList[1].push_back(2);
-    adjList[1].push_back(3);
-    adjList[2].push_back(5);
+    adjList[2].push_back(3);
+    adjList[3].push_back(5);
+    adjList[4].push_back(3);
+    adjList[4].push_back(5);
     adjList[5].push_back(6);
-    adjList[5].push_back(7);
-    adjList[3].push_back(4);
-    adjList[4].push_back(6);
+    adjList[7].push_back(4);
     dfs(adjList);
 
 

e38eec695c09517da4d9dba7de7fadbf1ed93663
prakashupes <85prakash2017@gmail.com>
2020-01-09 11:56:22 +0530

    code

--- Graphs/pract.cpp
+++ Graphs/pract.cpp
@@ -1,94 +1,33 @@
-
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-
-template <typename T>
-class Graph
-{
-    unordered_map<T,list<T>> adjList;
-    public:
-    void addEdge(T node1, T node2, bool bidir=false)
+int main() {
+    int n;
+	cin>>n;
+    int arr[n*2]={0};
+    int arr2[n];
+    for(int i=0;i<n;i++)
     {
-        adjList[node1].push_back(node2);
-        if(bidir)
-        {
-            adjList[node2].push_back(node1);
-        }
 
+        cin>>arr[i];
     }
-
-    void bfsTopological()
+    for(int i=0;i<n;i++)
     {
-        //map<T,bool>visited;
-        map<T,int> indegree;
-        queue<T>q;
 
-        //Initialize all inoreder 0
-        for(auto x:adjList)
-        {
-            T node=x.first;
-
-                indegree[node]=0;
-                //visited[node]=false;
-        }
-
-        //Add indegree for each nodes
-
-        for(auto x:adjList)
-        {
-            T node=x.first;
-            for(auto n:adjList[node])
-            {
-                indegree[n]++;
-            }
-        }
-
-        //insert nodes into queue with indegree of 0
-
-        for(auto x:adjList)
-        {
-            T node=x.first;
-            if(indegree[node]==0)
-            {
-                q.push(node);
-            }
-
-        }
-
-        while(!q.empty())
-        {
-            T node=q.front();
-            q.pop();
-            cout<<node<<" ";
-            //Reduce degree of neighbour of poped node
-
-            for(T x:adjList[node])
-            {
-                indegree[x]--;
-                if(indegree[x]==0)
-                {
-                    q.push(x);
-                }
-
-            }
-
-        }
-
+        cin>>arr2[i];
     }
+    int j=0;
+    for(int i=n;i<n*2;i++)
+    {
 
-};
+        arr[i]=arr2[j];
+        j++;
+    }
+    int m=0;
+    for(int i=0;i<n*2;i++)
+    {
 
-int main()
-{
-    Graph<int>g;
-    g.addEdge(1,2);
-    g.addEdge(1,4);
-    g.addEdge(2,3);
-    g.addEdge(4,3);
-    g.addEdge(7,4);
-    g.addEdge(3,5);
-    g.addEdge(4,5);
-    g.addEdge(5,6);
-    //g.bfsTopological();
+        m=m+arr[i];
+    }
+    cout<<m/(n*2);
 
 }

a2f30cc6fff7c7af60ad34ed0286c1d9e3d9705f
prakashupes <85prakash2017@gmail.com>
2020-01-19 17:45:13 +0530

    Disjoint Sets

---
+++ DisjointSet/021_PathCopression.cpp
@@ -0,0 +1,65 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int* parent;
+int* _rank;
+int find(int x)
+{
+    if(parent[x]==x)
+        return x;
+    parent[x]= find(parent[x]);
+    return parent[x];
+}
+
+void Union(int x,int y)
+{
+    int x_rep=find(x);
+
+    int y_rep=find(y);
+
+    if(x_rep==y_rep) //Both are in same set
+        return;
+
+    if(_rank[x_rep]<_rank[y_rep])
+    {
+
+
+        parent[x_rep]=y_rep;
+    }
+
+    else if(_rank[y_rep]<_rank[x_rep])
+    {
+
+        parent[y_rep]=x_rep;
+    }
+    else if(_rank[x_rep] == _rank[y_rep])
+    {
+
+        parent[y_rep]=x_rep;
+        _rank[x_rep]++;
+
+    }
+
+}
+int main()
+{
+    int n=5;
+    parent=new int[n];
+    _rank=new int[n];
+
+    for(int i=0;i<n;i++)
+    {
+        parent[i]=i;
+        _rank[i]=0;
+    }
+    Union(3,4);
+    Union(2,3);
+    Union(1,2);
+    Union(0,1);
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<parent[i]<<" "<<_rank[i]<<" "<<endl;
+
+    }
+}

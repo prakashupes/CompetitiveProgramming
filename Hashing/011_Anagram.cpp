04f6224de764b93732adafafc59047d241c24628
prakashupes <85prakash2017@gmail.com>
2020-03-27 18:20:38 +0530

    src

---
+++ Hashing/011_Anagram.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+template <typename Map>
+bool map_compare (Map const &lhs, Map const &rhs) {
+    // No predicate needed because there is operator== for pairs already.
+    return lhs.size() == rhs.size()
+        && std::equal(lhs.begin(), lhs.end(),
+                      rhs.begin());
+}
+bool anagram(string str1, string str2)
+{
+    if(str1.length()!=str2.length())
+    {
+        return false;
+    }
+    map <char,int> m1;
+    map <char,int> m2;
+    
+    for(int i=0;i<str1.length();i++)
+    {
+        m1[str1[i]]++;
+    }
+    for(int i=0;i<str2.length();i++)
+    {
+        m2[str2[i]]++;
+    }
+    for(auto x:m1)
+    {
+    	cout<<x.first<<" "<<x.second<<endl;
+    }
+    for(auto x:m2)
+    {
+    	cout<<x.first<<" "<<x.second<<endl;
+    }
+    return map_compare(m1,m2);
+}
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str1;
+	    cin>>str1;
+	    string str2;
+	    cin>>str2;
+	    if(anagram(str1,str2))
+	    {
+	        cout<<"YES"<<endl;
+	        
+	    }
+	    else
+	        cout<<"NO"<<endl;
+	}
+	/*
+	string str1="geeksforgeeks";
+	string str2="gksfprgeeksee";
+	if(anagram(str1, str2)) cout<<"YES";
+	else cout<<"NO";
+	*/
+	return 0;
+}

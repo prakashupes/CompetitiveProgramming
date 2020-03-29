fe35ccc62f3c3919c9225525679d39ca9a32d24c
prakashupes <85prakash2017@gmail.com>
2020-03-29 12:38:01 +0530

    src

---
+++ LinkedList/Practise/001_Middle_Of_List.cpp
@@ -0,0 +1,99 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+/* Function to get the middle of the linked list*/
+int getMiddle(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+    printf("%d\n", getMiddle(head));
+    }
+    return 0;
+}
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   // Your code here
+   int pos=1;
+   Node *curr=head;
+   while(curr->next!=NULL)
+   {
+       pos++;
+       curr=curr->next;
+   }
+  // cout<<pos<<endl;
+   if(pos%2==0)
+   {
+        pos=pos/2;
+        //pos++;
+   }
+   else
+   {
+       pos=pos/2;
+   }
+   while(pos>0)
+   {
+       head=head->next;
+       pos--;
+   }
+   return head->data;
+   
+   
+}
+

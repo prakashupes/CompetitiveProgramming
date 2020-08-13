df66a62d0bff3e2c2dd4ef45214c40fcd36011fb
prakashupes <85prakash2017@gmail.com>
2020-08-13 23:13:05 +0530

    floor

--- Tree/011_Floor.cpp
+++ Tree/011_Floor.cpp
@@ -24,26 +24,12 @@ int floor(Node *root,int k)
 
 }
 
-Node *recFloor (Node *root, Node* prev, int k)
-{
-	if(prev==NULL && root ==NULL) return NULL;
-	if(prev!=NULL && root ==NULL) return prev;
-	if(root->key==k) return root;
-	
-	if(root->key > k) return recFloor(root->left,root->left,k);
-	else return recFloor(root->right,root,k);
-
-}
 
 int main()
 {
 	BSTree tree;
 	tree.genrateTree();
-	//cout<<(tree.root)->key;
-	Node *res =recFloor(tree.root,tree.root,1);
-	if(res!=NULL)
-	cout<<res->key<<" ";
-	else cout<<"-1";
+	cout<<floor(tree.root,16);
 
 
 }

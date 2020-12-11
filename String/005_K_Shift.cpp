1af9eb0ae08eb556bbee0bcb95729adb0054d135
prakashupes <85prakash2017@gmail.com>
2020-12-11 23:48:25 +0530

    K shift

--- String/005_K_Shift.cpp
+++ String/005_K_Shift.cpp
@@ -3,24 +3,21 @@
 using namespace std;
 int main()
 {
-/*
+
     string s="hacker";
-    string shifted;
+    
     int n=s.length();
+    string shifted="";
     int k=3;
     for(int i=0;i<s.length();i++)
     {
         int index=(i+k)%n;
-        shifted.at(index)=s[i];
+        shifted+=s[index];
     }
-    for(int i=0;i<s.length();i++)
-    {
+    
         cout<<shifted;
-    }
-    */
-
-    string str="hello";
-    char str2[str.size()];
+    
+    
 
 
 

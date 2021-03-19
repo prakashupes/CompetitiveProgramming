0a341def877720d3586f087b7079b4d60d85c9c7
Prakash Tiwari <85prakash2017@gmail.com>
2021-03-19 15:26:21 +0000

    coin chnage normal

--- DP/practise/002_Coin_Optimized.cpp
+++ DP/practise/002_Coin_Optimized.cpp
@@ -4,11 +4,7 @@ int n;
 int main()
 {
     cin>>n;
-    int a=n/100;
-    int b=a+n%100/20;
-    int c=b+n%20/10;
-    int d=c+n%10/5;
-    int e=d+n%5;
-    cout<<e;
+    cout<< n/100+n/20%5+n/10%2+n/5%2+n%5;
+    
  
 }

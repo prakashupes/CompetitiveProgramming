56459751722fc6c3702774241cd6ce201523fe99
Prakash Tiwari <85prakash2017@gmail.com>
2021-03-28 18:15:00 +0000

    space optimized

--- DP/006_LCS.cpp
+++ DP/006_LCS.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-
 #include<algorithm>
 #define M 100
 #define N 100
@@ -7,6 +6,8 @@ using namespace std;
 //dp
 
 int max(int a,int b) {return a>b?a:b;}
+
+//LCS top down
 int lcs(int dp[][N],string s1, string s2, int m,int n) //4 3
 {
     if(dp[m][n]!=-1) return dp[m][n];
@@ -35,7 +36,7 @@ int lcs(int dp[][N],string s1, string s2, int m,int n) //4 3
 
 }
 
-
+//LCS Bottom up
 int lcs_2 (string s1, string s2, int m,int n)
 {
     int dp[m+1][n+1];
@@ -58,6 +59,7 @@ int lcs_2 (string s1, string s2, int m,int n)
 }
 
 
+//print the LCS
 void lcs_print(int dp[][N],string s1, string s2, int m,int n) //4 3
 {
    int i=m;
@@ -86,10 +88,50 @@ void lcs_print(int dp[][N],string s1, string s2, int m,int n) //4 3
    reverse(str.begin(),str.end());
    cout<<str<<endl;
 }
+
+
+//Space optimized
+void Space_opti(string s1,string s2)
+{
+    int n=s1.length();
+    int m=s2.length();
+
+    int dp[2][m+1];
+
+    int index=1;
+    for(int i=0;i<=n;i++)
+    {
+
+        index=(!index);
+       
+        for(int j=0;j<=m;j++)
+        {
+            if(i==0 || j==0)
+            {
+                dp[index][j]=0;
+            }
+            
+            else if(s1[i-1]==s2[j-1])
+            {
+                dp[index][j]=dp[1-index][j-1]+1;
+
+            }
+            else
+            {
+                dp[index][j]=max(dp[index][j-1],dp[1-index][j]);
+            }
+
+
+        }
+    }
+
+    cout<<dp[index][m]<<endl;
+}
+
 int main()
 
 {
-    string s1="ABCXYWYZQRTY",s2="ACWXYWQT";
+    string s1="ABCXYWYZQRTY",s2="ACWXYWQTY";
     
     int dp[M][N]={-1};
    for(int i=0;i<=s1.length();i++)
@@ -104,7 +146,9 @@ int main()
    // cout<<lcs_2(s1,s2,s1.length(),s2.length())<<endl;
 
 
-    lcs_print(dp,s1,s2,s1.length(),s2.length());
+   // lcs_print(dp,s1,s2,s1.length(),s2.length());
+   Space_opti(s1,s2);
+
 
 
 }

0840e09919079cd36ee2459b582ff1f3d587f9ae
Prakash Tiwari <85prakash2017@gmail.com>
2021-05-08 09:48:46 +0000

    [trie] insert and search

---
+++ trie/001_Trie.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+using namespace std;
+
+typedef struct TrieNode
+{
+    TrieNode *nodes[26];
+   bool isEnd;
+
+   TrieNode()
+   {
+       isEnd=false;
+       for(int i=0;i<26;i++)
+       {
+           nodes[i]=NULL;
+       }
+   }
+    
+} TrieNode;
+
+void insert(TrieNode *root, string word)
+{
+    TrieNode *ref=root;
+    for(int i=0;i<word.length();i++)
+    {
+        int index=word[i]-'a';
+        if(ref->nodes[index]==NULL)
+        {
+            ref->nodes[index]=new TrieNode();
+        }
+        ref=ref->nodes[index];
+    }
+    ref->isEnd=true;
+}
+
+bool search(TrieNode *root, string word)
+{
+    TrieNode *ref=root;
+    for(int i=0;i<word.length();i++)
+    {
+        int index=word[i]-'a';
+        if(ref->nodes[index]==NULL)
+        {
+            return false;
+        }
+        ref=ref->nodes[index];
+    }
+    return true;
+}
+
+int main()
+{
+    string keys[] = {"the", "a", "there",
+                    "answer", "any", "by",
+                     "bye", "their","geek","geeks","cut","cat","but","bat","bats" };
+    int n = sizeof(keys)/sizeof(keys[0]);
+  
+    struct TrieNode *root = new TrieNode();
+  
+    // Construct trie
+    for (int i = 0; i < n; i++)
+        insert(root, keys[i]);
+  
+    // Search for different keys
+    
+    search(root, "bat")? cout << "Yes\n" :
+                         cout << "No\n";
+    search(root, "ba")? cout << "Yes\n" :
+                           cout << "No\n";
+                           
+    return 0;
+}

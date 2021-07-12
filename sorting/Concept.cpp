62db5037ace34a934fd1a729e21b31ac90fcd373
Prakash Tiwari <85prakash2017@gmail.com>
2021-07-12 16:47:52 +0000

    [src] concept

---
+++ sorting/Concept.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+void swap2(T &a,T &b)
+{
+    
+}
+int main()
+{
+    int a=3,b=4;
+    cout<<"Address "<<&a<<endl;
+    swap2(a,b);
+}

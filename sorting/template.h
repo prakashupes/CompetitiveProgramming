4a73c97f2663fc5682adbd7c71f3cf88d351c4db
prakashupes <85praksh2017@gmail.com>
2021-07-26 09:15:17 +0530

    temp

---
+++ sorting/template.h
@@ -0,0 +1,7 @@
+#ifndef v
+#define v vector<int>
+#endif
+#define arr[] ar
+#define print_v(v) for(auto x:v) cout<<x<<" " 
+#define end cout<<endl
+#define print_a(ar,n) for(int ii=0;ii<n;ii++)
---
+++ template.h
@@ -0,0 +1,10 @@
+#ifndef template
+#define template
+#ifndef v
+#define v vector<int>
+#endif
+
+#define print_v(v) for(auto x:v) cout<<x<<" " 
+#define end cout<<endl
+#define print_a(int ar[],n) for(int ii=0;ii<n;ii++) cout<<ar[ii]<<" "
+#endif //template

1d8858165a00fcb8518aa0399028ba2bd090a1fa
prakashupes <85prakash2017@gmail.com>
2021-07-28 11:33:58 +0530

    revision

--- revision/001_graph_1.cpp
+++ revision/001_graph_1.cpp
@@ -87,6 +87,59 @@
 
 
  };
+
+template <class T>
+ class Graph_2{
+     unordered_map<T,list<T>> adjList;
+
+     public:
+     void insert(T u,T v,bool bid=true)
+     {
+         if(bid)
+         {
+             adjList[u].push_back(v);
+             adjList[v].push_back(u);
+         }
+     }
+
+
+ };
+
+ void helper(unordered_map<int,list<int>> adjList,unordered_map<int,bool> &vis,stack<int>& order,int src)
+{
+    vis[src]=true;
+
+    for(auto x: adjList[src])
+    {
+        if(!vis[x])
+        {
+            helper(adjList,vis,order,x);
+        }
+    }
+    order.push(src);
+}
+
+
+ void topological(unordered_map<int,list<int>> adjList)
+ {
+     unordered_map<int,bool> vis;
+     stack<int> order;
+
+     for(auto node: adjList)
+     {
+         if(!vis[node.first])
+         {
+             helper(adjList,vis,order,node.first);
+         }
+     }
+
+     while(!order.empty())
+     {
+         cout<<order.top()<<" ";
+         order.pop();
+     }
+     
+ }
  
  int main()
  {
@@ -98,9 +151,24 @@
      g.insert(1,3);
      g.insert(2,6);
      g.insert(4,5);
-     g.print_list(g);
-     g.bfs(g,0);
+    // g.print_list(g);
+    // g.bfs(g,0);
      cout<<endl;
-     g.dfs(0);
+     //g.dfs(0);
+
+
+
+
+//topological
+    unordered_map<int,list<int>> adjList;
+    adjList[1].push_back(4);
+    adjList[1].push_back(2);
+    adjList[2].push_back(3);
+    adjList[3].push_back(5);
+    adjList[4].push_back(3);
+    adjList[4].push_back(5);
+    adjList[5].push_back(6);
+    adjList[7].push_back(4);
+    topological(adjList);
 
  }
